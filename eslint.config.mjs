931229191605731381d4050bdceab13914f8d6f5
ottoneto <ottosouza.dev@gmail.com>
2025-01-25 23:11:28 -0300

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,12 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+  ...compat.config({
+    extends: ["next"],
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
     },
-  },
+  }),
 ];
 
 export default eslintConfig;
