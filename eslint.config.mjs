c4071cbf7ff4212bb37f2234e0ff8a8c99ddc697
ottoneto <ottosouza.dev@gmail.com>
2025-01-25 23:16:19 -0300

    finalizado

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,10 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next"],
+    extends: ["next", "plugin:@typescript-eslint/recommended"],
     rules: {
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
+      "@typescript-eslint/no-explicit-any": "warn", // Emite um aviso em vez de erro
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
     },
   }),
 ];
